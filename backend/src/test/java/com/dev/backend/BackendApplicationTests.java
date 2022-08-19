8dc785f30a6e039450b836cb05d348f0e5f2b310
Mthws167 <mthws.henrique@hotmail.com>
2022-08-19 01:09:49 -0300

    Loja Virtual

---
+++ backend/src/test/java/com/dev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BackendApplicationTests {
+
+	public static void main(String[] args){
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
