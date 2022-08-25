7e448eb922ad9d713d8acd0c122ce4eb4a4bbb16
Mthws167 <mthws.henrique@hotmail.com>
2022-08-25 20:17:40 -0300

    CRUD MARCA BACKEND

---
+++ backend/src/main/java/com/dev/backend/controller/MarcaController.java
@@ -0,0 +1,46 @@
+package com.dev.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.backend.entity.Marca;
+import com.dev.backend.service.MarcaService;
+
+@RestController
+@RequestMapping("api/marca")
+public class MarcaController {
+	
+	@Autowired
+	private MarcaService marcaService;
+	
+	@GetMapping("/")
+	public List<Marca> buscarTodos(){
+		return marcaService.buscarTodos();
+	}
+	
+	@PostMapping("/")
+	public Marca inserir(@RequestBody Marca objeto) {
+		return marcaService.inserir(objeto);
+	}
+	
+	@PutMapping("/")
+	public Marca alterar(@RequestBody Marca objeto) {
+		return marcaService.alterar(objeto);
+	}
+	
+	@DeleteMapping("/")
+	public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+		marcaService.excluir(id);
+		return ResponseEntity.ok().build();	
+		}
+}
