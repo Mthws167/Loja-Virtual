e7eb5e872607ddc187a297a3308f79ac32fbab7c
Mthws167 <mthws.henrique@hotmail.com>
2022-10-06 20:27:56 -0300

    Recuperação e Alteração de Senha

---
+++ backend/src/main/java/com/dev/backend/controller/PessoaGerenciamentoController.java
@@ -0,0 +1,28 @@
+package com.dev.backend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.backend.entity.Pessoa;
+import com.dev.backend.service.PessoaGerenciamentoService;
+
+@RestController
+@RequestMapping("/api/pessoa-gerenciamento")
+public class PessoaGerenciamentoController {
+
+	private PessoaGerenciamentoService pessoaGerenciamentoService;
+
+	@PostMapping("/senha-codigo")
+	public String recuperarCodigo(@RequestBody Pessoa pessoa) {
+		return pessoaGerenciamentoService.solicitarCodigo(pessoa.getEmail());
+	}
+
+	@PostMapping("/senha-alterar")
+	public String alterarSenha(@RequestBody Pessoa pessoa) {
+		return pessoaGerenciamentoService.alterarSenha(pessoa);
+	}	
+
+}
